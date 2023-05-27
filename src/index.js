278034fc4e55c74af38a4ba11f788bf7f69375bf
Tuan Luu <tuan.luu@asnet.com.vn>
2023-05-27 21:31:27 +0700

    Update comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+// Add comment
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
